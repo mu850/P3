e963511bf548c43209f16a2ca13a4f5ccb82794a
mu850 <mu7474674@gmail.com>
2025-12-13 09:23:47 +0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,15 @@
+const cacheName = 'chaki-cache-v1-final';
+const assets = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
